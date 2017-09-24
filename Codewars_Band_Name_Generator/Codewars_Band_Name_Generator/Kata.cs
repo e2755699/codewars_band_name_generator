9780519d01f74328e61e8ac9c279305955ff2de1
Dustin <e275566@gmail.com>
2017-09-24 16:31:47 +0800

    錯誤版本1

---
+++ Codewars_Band_Name_Generator/Codewars_Band_Name_Generator/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_Band_Name_Generator
+{
+    [TestClass]
+    public class Kata
+    {
+        private void AssertShouldBe(string expect, string input)
+        {
+            Assert.AreEqual(expect, GetBandName(input));
+        }
+
+        [TestMethod]
+        public void Input_bed_Should_The_Space_Bed()
+        {
+            AssertShouldBe("The Bed", "bed");
+        }
+
+        [TestMethod]
+        public void Input_knife_Should_The_Space_Knife()
+        {
+            AssertShouldBe("The Knife", "knife");
+        }
+
+        [TestMethod]
+        public void Input_system_Should_The_Space_System()
+        {
+            AssertShouldBe("The System", "system");
+        }
+
+        private string GetBandName(string noun)
+        {
+            var newBand = "The ";
+            newBand += noun.Replace(noun[0], noun[0].ToString().ToUpper()[0]);
+            return newBand;
+        }
+    }
+}
---
+++ Codewars_Band_Name_Generator/Codewars_Band_Name_Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Codewars_Band_Name_Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Codewars_Band_Name_Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88600268-787a-4f8c-b6b9-a1b3c5969810")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
